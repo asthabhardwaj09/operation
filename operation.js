fb15504b85794ea494551ab3786ba416723d741e
Astha Bhardwaj <asthabhardwaj529@gmail.com>
2024-10-26 23:06:52 +0530

    operation

---
+++ operation.js
@@ -0,0 +1,46 @@
+// let first_value =6
+// let second_value=56
+// let result=second_value-first_value
+// console.log(result)
+// console.log(first_value+second_value)
+// // let negvalue =-value
+// // console.log(negvalue);
+
+// console.log(2+2);
+// console.log(5-2);
+// console.log(2/2);
+// console.log(2*4);
+// console.log(9%3);
+// console.log(3**2);
+
+// let str1="astha"
+// let str2=" bhardwaj"
+// let str3=str1+str2
+// console.log(str3)
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+3+"2"+"3")
+console.log("1"+"2"+"3"+2+3)
+console.log("2+3+4"+5+4)
+
+console.log(+true);
+console.log(true);
+// let score=''
+// console.log(score);
+console.log('');
+console.log(+'');
+
+
+let num1, num2, num3
+let total_result= num1=num2=num3=2+2
+console.log(total_result);
+
+let GameCounter=100;
+console.log(++GameCounter);
+console.log(GameCounter++);
+
+
+
+
